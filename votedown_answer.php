041824afe88337558b4c5fca08f9ddfb482660ac
jungyh0218 <jungyh0218@gmail.com>
2015-12-20 10:57:19 +0900

    voteup/down for answers are implemented.

---
+++ votedown_answer.php
@@ -0,0 +1,26 @@
+<?php
+	$conn = mysqli_connect("localhost", "knucsewiki", "log102338", "knucsewiki") or die("Cannot connect to SQL Server");
+
+	//make query
+	$sql = "SELECT * FROM `answer_vote` where `answer_idx`=".$_GET['answer_id']." and `members_idx`=".$_GET['memberIdx'];
+	
+	$result = mysqli_query($conn, $sql);
+	$row_num = mysqli_num_rows($result);
+	//echo $row_num;
+	if($row_num){
+		echo '{"status" : "Duplicated"}';
+	}else{
+		$sql = "UPDATE `answer` SET `vote` = `vote` - 1 WHERE answer_id=".$_GET['answer_id'];
+		$result = mysqli_query($conn, $sql);
+		$sql = "INSERT INTO `answer_vote` VALUES(".$_GET['answer_id'].", ".$_GET['memberIdx'].")";
+		$result = mysqli_query($conn, $sql);
+		if(!$result){
+			echo '{"status": "ERROR"}';
+		}else{
+			echo '{"status" : "OK"}';
+		}
+	}
+	//$sql = "UPDATE `questions` SET `vote` = `vote` + 1 WHERE `question_id`=".$_GET['question_id'];
+	//$result = mysqli_query($conn, $sql);
+	
+?>
